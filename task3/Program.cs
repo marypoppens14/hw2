1330b7130977515986830f269ee2f8d86d200c1d
Maria Popova <marypoppens14@gmail.com>
2023-05-02 17:46:12 +0300

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает на вход цифру, обозначающую день недели
+// проверяет, является ли этот день выходным 
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 1 && a < 6)
+{
+    Console.WriteLine("Рабочий день");
+}
+    
+else if (a == 6 || a == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+    
+else
+{
+    Console.WriteLine("Такого дня недели нет");
+}
