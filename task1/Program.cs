057456ae62c293af4f8d31e1c1b87284fff756bf
Maria Popova <marypoppens14@gmail.com>
2023-05-02 14:54:40 +0300

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,7 @@
+﻿// принимает на вход трёхзначное число 
+// на выходе показывает вторую цифру этого числа
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+string str = Convert.ToString(a);
+Console.WriteLine($"Вторая цифра - {str[1]}");
