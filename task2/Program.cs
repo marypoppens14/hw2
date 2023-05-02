87529865193ec1ff78e20053bc558b2d334af234
Maria Popova <marypoppens14@gmail.com>
2023-05-02 15:06:27 +0300

    task 2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет
+
+int a = Convert.ToInt32(Console.ReadLine());
+string b = Convert.ToString(a);
+
+if (b.Length > 2)
+{
+    Console.WriteLine($"Третья цифра числа - {b[2]}");
+}
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
